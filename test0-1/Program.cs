695e48deb3ce4cec03e809ff7c3fc99a35674d52
星波 张 <1104298767@qq.com>
2017-09-06 13:09:32 +0800

    添加项目文件。

---
+++ test0-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test0_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tushu ts1=new tushu(1,"高数",100,"新华","张龙涛","2017");
+            tushu ts2 = new tushu(2, "英语", 101, "新华", "安玉海", "2016");
+            tushu ts3 = new tushu(3, "os", 102, "新华", "穆宇", "2015");
+            tushu ts4 = new tushu(4, "程序结构", 103, "新华", "贺军", "2014");
+            tushu ts5 = new tushu(5, "大物", 104, "新华", "陈龙", "2013");
+
+            tushu[] tushus = new tushu[5];
+            tushus[0] = ts1;
+            tushus[1] = ts2;
+            tushus[2] = ts3;
+            tushus[3] = ts4;
+            tushus[4] = ts5;
+
+            foreach (tushu tushu in tushus)
+            {
+                tushu.print();
+            }
+
+        }
+    }
+}
---
+++ test0-1/tushu.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test0_1
+{
+    class tushu
+    {
+        private int isbn;
+        private string name;
+        private int price;
+        private string chubanshe;
+        private string zuozhe;
+        private string chubanriqi;
+        public int Isbn
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public string Chubanshe
+        {
+            get { return chubanshe; }
+            set { chubanshe = value; }
+        }
+        public string Zuozhe
+        {
+            get { return zuozhe; }
+            set { zuozhe = value; }
+        }
+        public string Chubanriqi
+        {
+            get { return chubanriqi; }
+            set { chubanriqi = value; }
+        }
+
+        public void print(){
+        
+            Console.WriteLine("isbn:"+ this.Isbn
+                +"图书名称:"+this.Name+"单价:"+this.Price+"出版社:"+this.Chubanshe+"作者:"
+                +this.Zuozhe+
+                "出版日期:"+ this.Chubanriqi);
+        }
+        public tushu(int isbn,string name,int price,
+            string chubanshe,string zuozhe,string chubanriqi){
+                this.Isbn = isbn;
+                this.Name = name;
+                this.Price = price;
+                this.Chubanshe = chubanshe;
+                this.Zuozhe = zuozhe;
+                this.Chubanriqi = chubanriqi;
+        
+        }
+
+
+    }
+}
